27e72f68ba92afc707aa04b6ae744b8b265159a5
ggarm <ricostaantunes@gmail.com>
2010-07-02 17:09:54 +0100

    Added an even nicer colorscheme to vimperator.

---
+++ .config/vimperator/styles/styles.js
@@ -0,0 +1,35 @@
+var loaduserstyles = function() {
+  var userstyles = [
+    [
+      'wikipedia:minimal',
+      'en.wikipedia.org',
+      'wikipedia.css'
+    ],
+
+    [
+      'archlinux-forums:minimal',
+      'bbs.archlinux.org',
+      'archlinux.forum.css'
+    ],
+
+    [
+      'google:minimal',
+      'http://www.google.com/search*',
+      'google.css'
+    ]
+  ], styledir = io.getRuntimeDirectories('styles')[0].path;
+
+  for ([i, [name, filter, file]] in Iterator(userstyles)) {
+    styles.removeSheet(false, name);
+    styles.addSheet(false, name, filter, 
+    File(styledir+'/'+file).read());
+  }
+}
+
+loaduserstyles();
+
+commands.add(
+  ['loaduserstyles'],
+  'Load all user styles',
+  loaduserstyles
+);
